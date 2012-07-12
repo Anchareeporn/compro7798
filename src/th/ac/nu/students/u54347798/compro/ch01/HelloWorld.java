918116ef69c4f0092c7015c7b91611d5cf8435ba
Anchareeporn <kwan_mezilza@hotmail.com>
2012-07-12 16:01:17 +0700

    เพิ่ม "Programming is fun"

--- src/th/ac/nu/students/u54347798/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54347798/compro/ch01/HelloWorld.java
@@ -4,7 +4,6 @@ package th.ac.nu.students.u54347798.compro.ch01;
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("HelloWorld");
-        
+        System.out.println("Programming is fun");       
     }
-    
-}
+ }
