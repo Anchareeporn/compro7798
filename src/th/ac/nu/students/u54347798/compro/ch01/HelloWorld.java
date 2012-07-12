e2a2a4014b5efcd890ca532c26a2d18b0bddbeb2
Anchareeporn <kwan_mezilza@hotmail.com>
2012-07-12 15:58:31 +0700

    New HelloWorld

---
+++ src/th/ac/nu/students/u54347798/compro/ch01/HelloWorld.java
@@ -0,0 +1,10 @@
+
+package th.ac.nu.students.u54347798.compro.ch01;
+
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("HelloWorld");
+        
+    }
+    
+}
